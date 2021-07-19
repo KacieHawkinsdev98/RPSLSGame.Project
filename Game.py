69e5703a6ee10b287a3b22ae9b629b4a250a33a5
unknown <kaciehawkins1@gmail.com>
2021-07-19 09:53:53 -0600

    Added classes for Game,Player, AI, and Human

---
+++ Game.py
@@ -0,0 +1,11 @@
+
+class Game:
+
+ def __init__(self):
+    self.display_welcome = ""
+    self.game_rules = ""
+    self.player_options = ""
+    self.gesture_options = []
+    self.round_outcome = ""
+    self.display_winner = ""
+    
