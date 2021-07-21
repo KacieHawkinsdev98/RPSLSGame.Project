1102003c7beac12636bbd1b42210517968a6c2dc
Jenny Kay <jennykay@Jennys-MBP.attlocal.net>
2021-07-20 22:56:03 -0500

    Added AI, Human, and Player imports to be able to run the game later on

--- Game.py
+++ Game.py
@@ -1,11 +1,6 @@
+from AI import AI
+from Human import Human
+from Player import Player
 
-class Game:
 
- def __init__(self):
-    self.display_welcome = ""
-    self.game_rules = ""
-    self.player_options = ""
-    self.gesture_options = []
-    self.round_outcome = ""
-    self.display_winner = ""
     
