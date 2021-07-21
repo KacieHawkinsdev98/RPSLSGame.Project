d8077443b45efdc112c86dc248d45d3dd26b4657
Jenny Kay <jennykay@Jennys-MBP.attlocal.net>
2021-07-20 23:02:16 -0500

    Added display rule method so users can see the rules

--- Game.py
+++ Game.py
@@ -11,3 +11,21 @@ class Game:
       print(f"Welcome to the game of RPSLS")
       self.player1.choose_gesture()
       self.player2.choose_gesture()
+
+      self.display_rules()
+      self.player1.choose_gesture()
+      print(self.player1.chosen_gesture)
+      self.display_winner()
+
+    def display_rules(self):
+        print("Here are the rules to the game:")
+        print("Rock crushes Scissors")
+        print("Scissors cuts Paper")
+        print("Paper covers Rock")
+        print("Rock crushes Lizard")
+        print("Lizard poisons Spock")
+        print("Spock smashes Scissors")
+        print("Scissors decapitates Lizard") 
+        print("Lizard eats Paper")
+        print("Paper disproves Spock")
+        print("Spock vaporizes Rock")
